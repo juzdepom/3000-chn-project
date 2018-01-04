54444b73a98d2c2fb90ee1d43b0de9314f00880e
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-01-04 02:04:35 -0800

    added allCharacters.js to data folder.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,8 +2,13 @@
     <div style="text-align: center;">
         <h1>3000 Chinese Characters</h1>
         <h3>三千字</h3>
+        <div class="form-group">
+          <textarea class="form-control" v-model="text" rows="8" cols="80" placeholder="Paste Chinese characters here..."></textarea>
+        </div>
 
-        <textarea v-model="text" rows="8" cols="80"></textarea>
+
+        <p>I have a library of x cards</p>
+        <button class="btn btn-primary" @click="run">Run</button>
 
     </div>
 </template>
@@ -11,6 +16,7 @@
 
 
 <script>
+
 
     export default {
         data(){
@@ -20,6 +26,35 @@
         },
         computed: {
 
+        },
+        methods: {
+          run(){
+            var x = 0
+            var array = []
+            for(var i=0;i<ONE_THOUSAND.length;i++){
+              x++
+              console.log(x)
+              console.log(ONE_THOUSAND[i].character)
+              array.push(ONE_THOUSAND[i].character)
+            }
+            for(var i=0;i<TWO_THOUSAND.length;i++){
+              x++
+              console.log(x)
+              console.log(TWO_THOUSAND[i].character)
+              array.push(TWO_THOUSAND[i].character)
+            }
+            for(var i=0;i<THREE_THOUSAND.length;i++){
+              x++
+              console.log(x)
+              console.log(THREE_THOUSAND[i].character)
+              array.push(THREE_THOUSAND[i].character)
+            }
+
+            var str = JSON.stringify(array);
+            console.log(str)
+            console.log(array.length)
+
+          }
         }
     }
 </script>
