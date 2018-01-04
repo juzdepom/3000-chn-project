df6864b177b4b7f104e25490d2f43fbb70c717b3
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-01-04 01:33:01 -0800

    Successfully converted all the txt files to variables in the data folder.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,13 +4,19 @@
         <h3>三千字</h3>
 
         <textarea v-model="text" rows="8" cols="80"></textarea>
+
+        <textarea rows="8" cols="80">{{ processed }}</textarea>
         <!-- <button type="button" name="button"></button> -->
 
         <!-- <input type="text" v-model="text"> -->
     </div>
 </template>
 
+
+
 <script>
+    // console.log(data)
+
     export default {
         data(){
           return {
@@ -18,6 +24,11 @@
           }
         },
         computed: {
+          processed(){
+            var str = this.text
+            str = str.split('++')
+            return str
+          }
             // funds() {
             //     return this.$store.getters.funds;
             // }
