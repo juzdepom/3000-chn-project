e7d13f0ca24de6043e1c7e84b84c1bac0546686c
Julia Gao Miller <julia.gao.miller@gmail.com>
2018-01-04 00:31:23 -0800

    took vuejs-stocktrader and commented out all unnecessary code.

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div style="text-align: center;">
+        <h1>3000 Chinese Characters</h1>
+        <h3>三千字</h3>
+
+        <textarea v-model="text" rows="8" cols="80"></textarea>
+        <!-- <button type="button" name="button"></button> -->
+
+        <!-- <input type="text" v-model="text"> -->
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+          return {
+            text: ""
+          }
+        },
+        computed: {
+            // funds() {
+            //     return this.$store.getters.funds;
+            // }
+        }
+    }
+</script>
